ac5b0e6e7d43085f2e123d425d9d90d32f812bd3
Narasimha Vuppala <nraovuppala@gmail.com>
2024-10-17 20:22:52 +0530

    added school logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../mythri-images/logo.jpg'
 
 function Header() {
   return (
     <header>
-      <h1>Mythri Global School @ Suryapet</h1>
+       <h1>Mythri Global School @ Suryapet</h1>
+      <div >
+        <table>
+          <tr>
+            <td> </td>
+            <td> <img width={250} height={250} src={logo} alt="Logo" />;</td>
+            <td><h2>Contact us : +91-6304969359</h2></td>
+          </tr>
+        </table>
+     
+        
+         
+      </div>
+    
+     
       <nav>
         <Link to="/">Home</Link>
         <br/>
